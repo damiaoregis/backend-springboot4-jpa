48fba958106acb03bc99657bff52adfcce7d8521
damiaoregis <damiao.v.serrana@gmail.com>
2026-03-13 16:56:28 -0300

    Subtotal & total methods

--- src/main/java/com/meuportfolio/backend/entities/OrderItem.java
+++ src/main/java/com/meuportfolio/backend/entities/OrderItem.java
@@ -30,23 +30,6 @@ public class OrderItem implements Serializable {
 		this.quantity = quantity;
 		this.price = price;
 	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		OrderItem other = (OrderItem) obj;
-		return Objects.equals(id, other.id);
-	}
 	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
@@ -77,6 +60,27 @@ public class OrderItem implements Serializable {
 
 	public void setPrice(Double price) {
 		this.price = price;
+	}
+
+    public Double getSubTotal(){
+        return price * quantity;
+    }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
 	}
     
 }
