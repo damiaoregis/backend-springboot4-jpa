fe230c3395eb3866888225d0d5a8f2aba0e20741
damiaoregis <damiao.v.serrana@gmail.com>
2026-03-16 11:53:58 -0300

    User insert

--- src/main/java/com/meuportfolio/backend/resources/UserResource.java
+++ src/main/java/com/meuportfolio/backend/resources/UserResource.java
@@ -6,8 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 import com.meuportfolio.backend.entities.User;
 import com.meuportfolio.backend.services.UserService;
@@ -28,4 +33,11 @@ public class UserResource {
     	User user = service.findById(id);
     	return ResponseEntity.ok().body(user);
     }
+
+	@PostMapping
+	public ResponseEntity<User>insert(@RequestBody User obj){
+		obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
